195ce4c771997f5dfa69e9f27adefe58db5e6bb8
Meera Patel <patelm9@mskcc.org>
2020-04-24 16:06:22 -0400

    first commit

---
+++ R/write_warn_or_error_ticket.R
@@ -0,0 +1,31 @@
+#' Return and write any system errors and warnings for an expression to the working directory README
+#' @description This function is meant to catch errors to trace back to the steps in a workflow where things bottlenecked.
+#' @return If there are errors and warnings to report, they are returned in the console as well as in the working directory README.md.
+#' @param expr Expression to evaluate
+#' @importFrom secretary typewrite_error
+#' @importFrom secretary typewrite_warning
+#' @export
+
+write_warn_or_error_ticket <- function(expr)
+{
+    W <- NULL
+    w.handler <- function(w){ # warning handler
+        W <<- w
+        invokeRestart("muffleWarning")
+    }
+
+    output <-
+        list(
+            error = withCallingHandlers(tryCatch(expr,
+                                                  error = function(e) e),
+                                                    warning = w.handler),
+             warning = W)
+
+    if (!("error" %in% class(output$error))) {
+        error_message <- secretary::typewrite_error(output$error$message)
+    }
+
+    if (("warning" %in% class(output$warning))) {
+        warning_message <- secretary::typewrite_warning(output$warning$message)
+    }
+}
---
+++ README.md
@@ -0,0 +1,16 @@
+2020-04-24 15:47:10 	File Created
+
+# Typewrite Messages
+Timestamp:	2020-04-24 15:53:42
+Local R Script:	~/GitHub/Public-Packages/police/R/try_catch_vital_function.R
+Typewrite Message:	WARNING: cannot open file '/Users/patelm9/Desktop/log.txt': No such file or directory
+Timestamp:	2020-04-24 15:53:42
+Local R Script:	~/GitHub/Public-Packages/police/R/try_catch_vital_function.R
+Typewrite Message:	ERROR: cannot open the connection
+Timestamp:	2020-04-24 15:47:10
+Local R Script:	~/GitHub/Public-Packages/police/R/try_catch_vital_function.R
+Typewrite Message:	cannot open file '/Users/patelm9/Desktop/log.txt': No such file or directory
+Timestamp:	2020-04-24 15:47:10
+Local R Script:	~/GitHub/Public-Packages/police/R/try_catch_vital_function.R
+Typewrite Message:	cannot open the connection
+
